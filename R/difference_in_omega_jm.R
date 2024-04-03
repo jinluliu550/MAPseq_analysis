9b9a3eb702622a87ad420edf368b1892302c9718
jinluliu550 <57995519+jinluliu550@users.noreply.github.com>
2024-04-03 10:11:04 +0100

    commit 2

--- R/difference_in_omega_jm.R
+++ R/difference_in_omega_jm.R
@@ -79,7 +79,49 @@ difference_in_omega_jm <- function(mcmc_run_omega_output){
   # Select the cluster and data with different p_jd
   df0_significant <- df0[which(df0$prob > 0.95 | df0$prob < 0.05),]
 
+  # Plot for each data
+  plot.for.each.data <- lapply(1:M,
+                               function(m){
+                                 
+                                 df0_all_m <- df0_all %>%
+                                   filter(data1 == m | data2 == m)
+                                 
+                                 # Always put m in front
+                                 for(i in 1:nrow(df0_all_m)){
+                                   
+                                   if(df0_all_m$data1[i] != m){
+                                     
+                                     df0_all_m$data2[i] <- df0_all_m$data1[i]
+                                     df0_all_m$data1[i] <- m
+                                     df0_all_m$prob[i] <- 1-df0_all_m$prob[i]
+                                   }
+                                   
+                                 }
+                                 
+                                 df1_all_m <- data.frame(cluster = factor(paste('cluster', df0_all_m$cluster),
+                                                                          levels = paste('cluster', 1:J)),
+                                                         mouse.pair = paste('(', df0_all_m$data1, ',', df0_all_m$data2, ')', sep = ''),
+                                                         probability = df0_all_m$prob)
+                                 
+                                 
+                                 probability_plot_m <- df1_all_m %>%
+                                   ggplot(mapping = aes(x = cluster,
+                                                        y = mouse.pair,
+                                                        fill = probability))+
+                                   geom_tile()+
+                                   theme_bw()+
+                                   scale_fill_gradientn(colours = c('white', 'yellow', 'red'),
+                                                        values = scales::rescale(c(0, 0.25, 1)),
+                                                        limits = c(0,1))+
+                                   xlab('cluster')+
+                                   ylab('mouse pairs')+
+                                   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+                                 
+                                 probability_plot_m
+                               })
+  
   return(list('all_obs' = df0_all,
+              'plot.for.each.data' = plot.for.each.data,
               'significant_obs' = df0_significant,
               'probability_plot' = probability_plot))
 
--- jinlu/data_run_hans.R
+++ jinlu/data_run_hans.R
@@ -5,3 +5,21 @@ Hans_data3 <- read.csv('./data/Han-data/han_brain6_gh.csv')
 data_Hans <- list(t(Hans_data1),
                   t(Hans_data2),
                   t(Hans_data3))
+
+
+mcmc_all_hans <- mcmc_run_all(Y = data_Hans,
+                             J = 20,
+                             number_iter = 8000,
+                             thinning = 5,
+                             burn_in = 3000,
+                             adaptive_prop = 0.1,
+                             print_Z = TRUE,
+                             
+                             
+                             a_gamma = 300,
+                             b_gamma = 10,
+                             a_alpha = 1/5,
+                             b_alpha = 1/2,
+                             num.cores = 10)
+
+
