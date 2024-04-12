98118ff5d4a9d227f14aaaacff583ddd199442c9
jinluliu550 <57995519+jinluliu550@users.noreply.github.com>
2024-04-12 10:14:21 +0100

    commit 2

--- sara/mouse_comparisons_tv.R
+++ sara/mouse_comparisons_tv.R
@@ -12,6 +12,126 @@ mytv_dist = function(x,ind){
   return(0.5*colSums(abs(x-y)))
 }
 
+m =  1 # index of mouse
+tv_dist = lapply(mcmc_all_EC8$omega_J_M_output,mytv_dist, ind = m )
+tv_dist <- data.frame(matrix(unlist(tv_dist), nrow=length(tv_dist), byrow=TRUE))
+names(tv_dist) = c("1", "2", "3", "4", "5", "6")
+tv_dist = tv_dist[-m]
+
+tv_dist = tv_dist %>% 
+  pivot_longer(
+    cols = everything(),
+    names_to = "Mouse", 
+    values_to = "TV"
+  )
+
+
+group.colors <- c('1'= "#FF3000",'2'= "#FF9900",'3'= "#FFDB6D",'4'= "#009999",'5'= "#333BFF",'6'= "#9633FF")
+
+ggplot(tv_dist) +
+  geom_density(aes(x=TV,col=Mouse, fill=Mouse), alpha=0.25) +
+  ggtitle(paste('Mouse',m,'comparisons')) + 
+  theme_bw()+
+  scale_fill_manual(values=group.colors) +
+  scale_color_manual(values=group.colors)
+
+
+m =  2 # index of mouse
+tv_dist = lapply(mcmc_all_EC8$omega_J_M_output,mytv_dist, ind = m )
+tv_dist <- data.frame(matrix(unlist(tv_dist), nrow=length(tv_dist), byrow=TRUE))
+names(tv_dist) = c("1", "2", "3", "4", "5", "6")
+tv_dist = tv_dist[-m]
+
+tv_dist = tv_dist %>% 
+  pivot_longer(
+    cols = everything(),
+    names_to = "Mouse", 
+    values_to = "TV"
+  )
+
+
+group.colors <- c('1'= "#FF3000",'2'= "#FF9900",'3'= "#FFDB6D",'4'= "#009999",'5'= "#333BFF",'6'= "#9633FF")
+
+ggplot(tv_dist) +
+  geom_density(aes(x=TV,col=Mouse, fill=Mouse), alpha=0.25) +
+  ggtitle(paste('Mouse',m,'comparisons')) + 
+  theme_bw()+
+  scale_fill_manual(values=group.colors) +
+  scale_color_manual(values=group.colors)
+
+
+m =  3 # index of mouse
+tv_dist = lapply(mcmc_all_EC8$omega_J_M_output,mytv_dist, ind = m )
+tv_dist <- data.frame(matrix(unlist(tv_dist), nrow=length(tv_dist), byrow=TRUE))
+names(tv_dist) = c("1", "2", "3", "4", "5", "6")
+tv_dist = tv_dist[-m]
+
+tv_dist = tv_dist %>% 
+  pivot_longer(
+    cols = everything(),
+    names_to = "Mouse", 
+    values_to = "TV"
+  )
+
+
+group.colors <- c('1'= "#FF3000",'2'= "#FF9900",'3'= "#FFDB6D",'4'= "#009999",'5'= "#333BFF",'6'= "#9633FF")
+
+ggplot(tv_dist) +
+  geom_density(aes(x=TV,col=Mouse, fill=Mouse), alpha=0.25) +
+  ggtitle(paste('Mouse',m,'comparisons')) + 
+  theme_bw()+
+  scale_fill_manual(values=group.colors) +
+  scale_color_manual(values=group.colors)
+
+
+m =  4 # index of mouse
+tv_dist = lapply(mcmc_all_EC8$omega_J_M_output,mytv_dist, ind = m )
+tv_dist <- data.frame(matrix(unlist(tv_dist), nrow=length(tv_dist), byrow=TRUE))
+names(tv_dist) = c("1", "2", "3", "4", "5", "6")
+tv_dist = tv_dist[-m]
+
+tv_dist = tv_dist %>% 
+  pivot_longer(
+    cols = everything(),
+    names_to = "Mouse", 
+    values_to = "TV"
+  )
+
+
+group.colors <- c('1'= "#FF3000",'2'= "#FF9900",'3'= "#FFDB6D",'4'= "#009999",'5'= "#333BFF",'6'= "#9633FF")
+
+ggplot(tv_dist) +
+  geom_density(aes(x=TV,col=Mouse, fill=Mouse), alpha=0.25) +
+  ggtitle(paste('Mouse',m,'comparisons')) + 
+  theme_bw()+
+  scale_fill_manual(values=group.colors) +
+  scale_color_manual(values=group.colors)
+
+
+m =  5 # index of mouse
+tv_dist = lapply(mcmc_all_EC8$omega_J_M_output,mytv_dist, ind = m )
+tv_dist <- data.frame(matrix(unlist(tv_dist), nrow=length(tv_dist), byrow=TRUE))
+names(tv_dist) = c("1", "2", "3", "4", "5", "6")
+tv_dist = tv_dist[-m]
+
+tv_dist = tv_dist %>% 
+  pivot_longer(
+    cols = everything(),
+    names_to = "Mouse", 
+    values_to = "TV"
+  )
+
+
+group.colors <- c('1'= "#FF3000",'2'= "#FF9900",'3'= "#FFDB6D",'4'= "#009999",'5'= "#333BFF",'6'= "#9633FF")
+
+ggplot(tv_dist) +
+  geom_density(aes(x=TV,col=Mouse, fill=Mouse), alpha=0.25) +
+  ggtitle(paste('Mouse',m,'comparisons')) + 
+  theme_bw()+
+  scale_fill_manual(values=group.colors) +
+  scale_color_manual(values=group.colors)
+
+
 m =  6 # index of mouse
 tv_dist = lapply(mcmc_all_EC8$omega_J_M_output,mytv_dist, ind = m )
 tv_dist <- data.frame(matrix(unlist(tv_dist), nrow=length(tv_dist), byrow=TRUE))
@@ -29,11 +149,13 @@ tv_dist = tv_dist %>%
 group.colors <- c('1'= "#FF3000",'2'= "#FF9900",'3'= "#FFDB6D",'4'= "#009999",'5'= "#333BFF",'6'= "#9633FF")
 
 ggplot(tv_dist) +
-  geom_density(aes(x=TV,col=Mouse, fill=Mouse),, alpha=0.25) +
+  geom_density(aes(x=TV,col=Mouse, fill=Mouse), alpha=0.25) +
   ggtitle(paste('Mouse',m,'comparisons')) + 
   theme_bw()+
   scale_fill_manual(values=group.colors) +
   scale_color_manual(values=group.colors)
+
+
 
 # Compute the posterior mean
 tv_mean = matrix(0, 6,6)
